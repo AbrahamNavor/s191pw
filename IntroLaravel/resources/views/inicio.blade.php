5a5737581ae112c382f07fb2f39ba925acffd0e5
Abraham Navor <122043373@upq.edu.mx>
2024-09-27 20:30:21 -0600

    Finalizacion IntroLaravel
    Se concluyo la practica donde estuvimos indagando en los temas de como trabajar en el entorno de laravel, creando una pagina de index sencilla, con su formulario de registro y su plantilla de consultas.

--- IntroLaravel/resources/views/inicio.blade.php
+++ IntroLaravel/resources/views/inicio.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,14 +9,17 @@
     @vite(['resources/js/app.js'])
 
     <style>
-        body, html {
+        body,
+        html {
             height: 100%;
         }
+
         .full-height {
             height: 100vh;
         }
     </style>
 </head>
+
 <body>
 
     <div class="d-flex flex-column justify-content-center align-items-center text-center full-height">
@@ -27,6 +31,7 @@
         <a href="{{ route('rutacacas') }}" class="btn btn-primary"> Ir al Registro</a>
 
     </div>
-    
+
 </body>
+
 </html>
