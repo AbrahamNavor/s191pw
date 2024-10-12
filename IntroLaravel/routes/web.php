c43136ab7f876497173260106d3beb0c0acd3e7f
Abraham Navor <122043373@upq.edu.mx>
2024-10-11 20:23:23 -0600

    Actividad 4 Controladores
    En esta actividad se hizo la practica de controladores en el proyecto introlaravel, donde se modifico el codigo de las rutas dentro de web.php para poder hacer uso de esta herramienta

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorvistas;
+
+Route::get('/', [controladorvistas::class, 'home'])->name('rutainicio');
+Route::get('/form', [controladorvistas::class, 'insert'])->name('rutacacas');
+route::get('/consultar', [controladorvistas::class, 'select'])->name('rutaconsulta');
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
-Route::view('/', 'inicio')->name('rutainicio');
+/* Route::view('/', 'inicio')->name('rutainicio');
 
 Route::view('/form', 'formulario')->name('rutacacas');
 
-Route::view('/consultar', 'clientes')->name('rutaconsulta');
+Route::view('/consultar', 'clientes')->name('rutaconsulta'); */
 
 Route::view('/component', 'componentes')->name('rutacomponent');
