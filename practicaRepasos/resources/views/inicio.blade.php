e490d44c781de91ecb7f4ec0c53c04ed6ba4b0b8
Abraham Navor <122043373@upq.edu.mx>
2024-10-18 10:00:12 -0600

    Commint #3
    Se creo una vista de inicio al estilo de una portada con nuestros datos individuales

---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.nav')
+
+@section('titulo', 'Portada - Practica Repasos')
+
+@section('contenido1')
+    <div class="container">
+        <header>
+            
+        </header>
+
+        <div class="jumbotron text-center mt-5">
+            <h1 class="display-4">Bienvenido a Practica Repasos</h1>
+            <p class="lead">Este es un sitio para realizar repasos vistos en clases. Universidad Politécnica de Querétaro. Pérez Navor Abraham - s191.</p>
+            <a class="btn btn-primary btn-lg mt-3" href="" role="button">REPASO 1</a>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Título #1</h2>
+                <p>Se vendeeeeeeeeeeen tamaleeeeeeeeeeees oaxaqueñooooooooos.</p>
+                <p>Compraaaamos carrooos y camionetaaas viejaaaas para desarmaaar</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Título #2</h2>
+                <p>Halls, Clorets, pepitorias, cacahuate
+                    Tridents, chocolate, Mazapanes, cigarros y Tic Tac
+                    Rosas, bolas y tamal
+                    Nueces y pitachios
+                    Cocadas de coco y plátanos asados
+                    Pingüinos, abanicos y Pelones
+                    Ranas y Panditas
+                    Motos, burbujas y carros</p>
+            </div>
+            <div class="col-md-4">
+                <h2>Título #3</h2>
+                <p>Jamaica colombiana
+                    Tarjeta Telcel, Tarjeta Movistar, Tarjeta Multifon
+                    Bombones del América y las Chivas
+                    ¿Y que te voy a dar?
+                    Taka, tataka, taka-taka-ta</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+@endsection
---
+++ practicaRepasos/resources/views/layouts/nav.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @vite(['resources/js/app.js'])
+    <title> @yield('titulo') </title>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Practica Repasos</a>
+        <button class="navbar-toggler" type="button">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navegadorInicio">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Nosotros</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Servicios
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Desarrollos</a>
+                        <a class="dropdown-item" href="#">Diseño</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Consultoría</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Buscador" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            </form>
+        </div>
+    </nav>
+
+    @yield('contenido1')
+
+</body>
+</html>
