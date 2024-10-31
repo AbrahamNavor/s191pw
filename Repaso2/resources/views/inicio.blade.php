b59eb8fa21a9a36e7b9b863e1d5768898a5c730a
Abraham Navor <122043373@upq.edu.mx>
2024-10-31 12:46:01 -0600

    Commint #1 Plantilla Principal
    Creación de la Plantilla principal agregando el nav desde una plantilla diferente y tambien se incorporo el boton(no funcional) para redirigir a la siguiente vista

---
+++ Repaso2/resources/views/inicio.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Inicio')
+
+@section('contenido')
+
+   <div class="container mt-5 text-center">
+        <h1>Noticia Literaria</h1>
+        <p>El Día de las Bibliotecas se celebra cada 24 de octubre desde el año 1997. Este día nació como iniciativa de la Asociación
+             Española de Amigos del Libro Infantil y Juvenil con el objetivo de concienciar a la sociedad de la importancia de la lectura
+              y como homenaje y reconocimiento a la labor de los bibliotecarios/as.</p>
+              <img alt="imagen aleatoria" src="https://picsum.photos/700/400?random">
+    </div>
+    <div class="d-flex justify-content-center">
+        <a href="" class="btn btn-success mt-3 w-50 text-center">Ir a otra vista</a>
+    </div>
+
+@endsection
---
+++ Repaso2/resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    @vite(['resources/js/app.js'])
+    <title>@yield('titulo')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Biblioteca</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Noticias</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    @yield('contenido')
+
+    <footer class="bg-light text-center text-lg-start mt-5">
+        <div class="container p-4">
+            <h5 class="card-title text-center">©Biblioteca Navor</h5>
+            <p class="text-center">{{ date('d M Y') }}</p>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
