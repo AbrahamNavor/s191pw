2aba3a184044bb95be5785d67709e49384ea7d0a
Abraham Navor <122043373@upq.edu.mx>
2024-10-30 20:30:11 -0600

    Practica 6.1
    Se realizo la actividad de traductor de idiomas ajustando todo el proyecto para traducir

--- IntroLaravel/resources/views/layouts/plantilla1.blade.php
+++ IntroLaravel/resources/views/layouts/plantilla1.blade.php
@@ -26,10 +26,10 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('rutacacas')?"text-warning":"" }}">Registro Clientes</a>
+                <a class="nav-link {{ request()->routeIs('rutacacas')?"text-warning":"" }}">{{ __('Registro Clientes')}}</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link {{ request()->routeIs('rutaconsulta')?"text-success":"" }}">Consultar Clientes</a>
+                <a class="nav-link {{ request()->routeIs('rutaconsulta')?"text-success":"" }}">{{ __('Consultar Clientes')}}</a>
               </li>
             </ul>
           </div>
