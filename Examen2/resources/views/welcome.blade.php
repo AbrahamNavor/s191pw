1b5d571c57d42018e5b075b512efb2b7c5ef7a37
Abraham Navor <122043373@upq.edu.mx>
2024-11-07 13:51:39 -0600

    Commint Final
    Se concluyo el proyecto Turista sin Maps para su revision

--- Examen2/resources/views/welcome.blade.php
+++ Examen2/resources/views/welcome.blade.php
@@ -12,15 +12,24 @@
     @csrf
         <div>
             <label for="nombre">Nombre:</label>
-            <input type="text" name="txtnombre" >
+            <input type="text" name="txtnombre" value="{{ old('txtnombre') }}">
+            @if ($errors->has('txtnombre'))
+                <small>{{ $errors->first('txtnombre') }}</small>
+            @endif
         </div>
         <div>
             <label for="marca">Marca:</label>
-            <input type="text" name="txtmarca">
+            <input type="text" name="txtmarca" value="{{ old('txtmarca') }}">
+            @if ($errors->has('txtmarca'))
+                <small>{{ $errors->first('txtmarca') }}</small>
+            @endif
         </div>
         <div>
             <label for="cantidad">Cantidad:</label>
-            <input type="number"  name="txtcantidad">
+            <input type="number" name="txtcantidad" value="{{ old('txtcantidad') }}">
+            @if ($errors->has('txtcantidad'))
+                <small>{{ $errors->first('txtcantidad') }}</small>
+            @endif
         </div>
         <div>
             <button type="submit">Enviar</button>
